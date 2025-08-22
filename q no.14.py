8246484ad7b654f323e9308b96e57e1d2ffc9e6c
mariaabdullahh23-dot <mariaabdullahh23@gmail.com>
2025-08-22 22:43:50 +0500

    Create q no.14.py

---
+++ q no.14.py
@@ -0,0 +1,7 @@
+Total = int (input ("Enter total number of questions"))
+
+correct = int (input ("Enter number  of correct answer  :")) 
+
+Percentage (correct*100) // total
+
+Print ("Percentage  score  = ", Percentages ,"%")
