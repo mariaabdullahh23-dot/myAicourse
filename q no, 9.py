0c50fb44880e2cc9688117e3bb438ec5ad660c18
mariaabdullahh23-dot <mariaabdullahh23@gmail.com>
2025-08-22 22:37:13 +0500

    Create q no, 9.py

---
+++ q no, 9.py
@@ -0,0 +1,14 @@
+sub1 = int(input("enter no of 1 subj"))
+sub2 = int(input("enter no of 2 subj"))
+sub3 = int(input("enter no of 3 subj"))
+sub4 = int(input("enter no of 4 subj"))
+sub5 = int(input("enter no of 5 subj"))
+
+
+Tm = sub1+sub2+sub3+sub4+sub5
+average = Tm/5
+percentage =(Tm*100) /500
+
+print (f"total marks{Tm}")
+print (f"average{average}")
+print(f"percentage {percentage}")
