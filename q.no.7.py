1df90b7a39781e78cac3f19280647fc1fc19531d
mariaabdullahh23-dot <mariaabdullahh23@gmail.com>
2025-08-22 22:33:14 +0500

    Create q.no.7.py

---
+++ q.no.7.py
@@ -0,0 +1,9 @@
+n = int(input("enter number of candes"))
+k = int(input("enter number of students"))
+
+get = n/k
+leftover = n%k
+
+
+print (f"each students {get} candles")
+print (f"each candles left are {leftover77}")
