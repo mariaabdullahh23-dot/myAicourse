9a7cae31cf8e8266f215ed8ffa6ec4b714d6521c
mariaabdullahh23-dot <mariaabdullahh23@gmail.com>
2025-08-28 23:55:22 +0500

    Create tuple manipulation.py

---
+++ tuple manipulation.py
@@ -0,0 +1,14 @@
+t1 = (4, 6, 9)
+t2 = (3, 5, 7)
+
+t1, t2 = t2, t1
+print("t1:", t1)
+print("t2:", t2)
+
+t = (5, 10, 15, 20)
+value = t[1]
+print(value)
+
+t = (1, 2, 3, 4, 5)
+rev_t = t[::-1]
+print(rev_t)
