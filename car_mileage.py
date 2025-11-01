ad973d5c9e4529063340a045076f68e916be58c2
mariaabdullahh23-dot <mariaabdullahh23@gmail.com>
2025-11-01 11:52:44 +0500

    house price

---
+++ car_mileage.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+df = pd.read_csv("car_mileage.csv")
+print(df.shape)
+print(df.head())
+
+plt.figure(figsize=(8,6))
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
+plt.title("Feature Correlation with Mileage")
+plt.show()
+print(input("Wait for me..."))
+
+X = df.drop(columns=['Mileage_kmpl', 'Car_ID'])
+Y = df['Mileage_kmpl']
+
+X = pd.get_dummies(X, drop_first=True)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=42, train_size=0.8)
+print("Train/Test Split:", x_train.shape[0], "/", x_test.shape[0])
+
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+model = RandomForestRegressor(random_state=42, n_estimators=100)
+model.fit(x_train_scaled, y_train)
+
+
+y_pred = model.predict(x_test)
---
+++ house price.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+df = pd.read_csv("house_prices.csv")
+print(df.shape)
+print(df.head())
+
+plt.figure(figsize=(8,6))
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
+plt.title("Feature Correlation with Price")
+plt.show()
+print(input("Wait for me..."))
+
+X = df.drop(columns=['Price', 'House_ID'])
+Y = df['Price']
+
+X = pd.get_dummies(X, drop_first=True)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, random_state=42, train_size=0.8)
+print("Train/Test Split:", x_train.shape[0], "/", x_test.shape[0])
+
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_test_scaled = scaler.transform(x_test)
+
+model = RandomForestRegressor(random_state=42, n_estimators=100)
+model.fit(x_train_scaled, y_train)
+
+
+y_pred = model.predict(x_test_scaled)
+
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error (MSE): {round(mse, 2)}")
+print(f"Mean Absolute Error (MAE): {round(mae, 2)}")
+print(f"R² Score: {round(r2, 3)}")
+
+
+plt.figure(figsize=(6,5))
+sns.scatterplot(x=y_test, y=y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs Predicted House Prices")
+plt.show()
+
+print(input("Wait for me..."))
